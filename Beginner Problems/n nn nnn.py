82382f1e226895bcdc57e93332ae5bbe3868f959
Logan <logankrause01@gmail.com>
2022-07-31 16:43:02 -0400

    Adding

--- Beginner Problems/n nn nnn.py
+++ Beginner Problems/n nn nnn.py
@@ -1,1 +1,13 @@
 def main():
+    x = int(input("Enter a single digit integer: "))
+
+    print(n_nn_nnn(x))
+
+def n_nn_nnn(n):
+    n1 = int("{}".format(n))
+    n2 = int("{}{}".format(n,n))
+    n3 = int("{}{}{}".format(n,n,n))
+
+    result = n1 + n2 + n3
+    return result
+main()
