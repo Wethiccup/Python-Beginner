4962b59bda6944799c7bfbcfabbe9e7766f9cc4a
Logan <logankrause01@gmail.com>
2022-07-31 17:17:49 -0400

    Adding

---
+++ Beginner Problems/Add 'ls' to String.py
@@ -0,0 +1,12 @@
+def new_string(str):
+    if len(str) >= 2 and str[:2] == "Is":
+        return str
+    else:
+        return "Is" + str
+
+def main():
+    string = input("Enter a string: ")
+
+    print(new_string(string))
+
+main()
