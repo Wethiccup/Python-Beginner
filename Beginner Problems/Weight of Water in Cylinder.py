0876cf8467ab88402c4915b5d2a2a3884ba828f3
Logan <logankrause01@gmail.com>
2022-09-01 23:07:00 -0400

    yo

---
+++ Beginner Problems/Weight of Water in Cylinder.py
@@ -0,0 +1,19 @@
+import math
+
+def main():
+    x = float(input("Enter the radius (cm) of the cylinder: "))
+    y = float(input("Enter the height (cm) of the cylinder: "))
+
+    print(weight(x, y))
+
+def weight(r, h):
+
+    rmeter = float(r/100)
+    hmeter = float(h/100)
+
+    volume = (math.pi) * (rmeter ** 2) * hmeter
+    mass = 1000 * volume
+
+    return mass
+
+main()
