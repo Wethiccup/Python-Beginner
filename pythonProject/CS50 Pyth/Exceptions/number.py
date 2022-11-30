789e15af7b7c5f4d36b6b70678b5472aeca9db6d
Logan <logankrause01@gmail.com>
2022-11-29 22:20:42 -0500

    11/29 Progress

--- pythonProject/CS50 Pyth/Exceptions/number.py
+++ pythonProject/CS50 Pyth/Exceptions/number.py
@@ -1,7 +1,44 @@
-try:
-    x = int(input("What's x? "))
-    print(f"x is {x}")
-except ValueError:
-    print("x is not an integer")
+# try:
+#     x = int(input("What's x? "))
+# except ValueError:
+#     print("x is not an integer")
+# else:
+#     print(f"x is {x}")
 
-10:50 lecture
+# while True:  # Adds a way to loop the function until an integer is called
+#     try:
+#         x = int(input("What's x? "))
+#     except ValueError:
+#         print("x is not an integer")
+#     else:
+#         break  # Break out of loop because integer was called and else is used
+#
+# print(f"x is {x}")
+
+# def main():
+#     x = get_int()
+#     print(f"x is {x}")
+#
+# def get_int():
+#     while True:  # Adds a way to loop the function until an integer is called
+#         try:
+#             return int(input("What's x? "))  # Return also works as a break and returns the value.
+#             # Can put into one line without defining variable
+#         except ValueError:
+#             print("x is not an integer")
+#
+# main()
+
+def main():
+    x = get_int("What's x? ")
+    print(f"x is {x}")
+
+def get_int(prompt):
+    while True:  # Adds a way to loop the function until an integer is called
+        try:
+            return int(input(prompt))  # Return also works as a break and returns the value.
+            # Can put into one line without defining variable
+        except ValueError:
+            pass  # Still catches the error but "passes" on saying anything; just loops
+
+main()
