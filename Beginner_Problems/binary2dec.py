430f54bd8f6b9732ddd4ad3911c3f41dd76a9f60
Logan <logankrause01@gmail.com>
2023-01-17 23:20:56 -0500

    1/17/23

--- Beginner_Problems/binary2dec.py
+++ Beginner_Problems/binary2dec.py
@@ -1,30 +1,31 @@
-from int_to_list import int_list
-
-
 def main():
     print("1. Binary to Decimal\n2. Decimal to Binary")
 
     decision = input("Pick an option: ")
 
     if decision == "1":
-        n = int(input("Enter binary: "))
-        binary(n)
+        n = input("Enter binary: ")
+        binary2dec(n)
     if decision == "2":
-        n = int(input("Enter a decimal: "))
-        decimal(n)
+        n = input("Enter a decimal: ")
+        decimal2bin(n)
 
 
-def binary(x):
-    new = int_list(x)
-    print(new)
-    for i in range(x):
-        if i != 0 and 1:
-            print("What is entered is not binary")
-            exit()
-        else:
-            for i in range(x):
-                if int_list(x):
-                    return
+def binary2dec(x):
+    num_list = [0]
+    num_list = [eval(i) for i in x]
+    print(x)
+    print(num_list)
+    # for i in range(len(num_list)):
+    if all(num_list) != "1" or "0":
+        print(num_list)
+        print("What is entered is not binary")
+
+    else:
+        print("balls")
+        for i in range(x):
+            if int_list(x):
+                return
 
 
 if __name__ == "__main__":
---
+++ Beginner_Problems/testing.py
@@ -0,0 +1,5 @@
+str = "0110100110011"
+
+num_list = [eval(i) for i in str]
+
+print(num_list)
